5483d2e230a918493f126c9007a6750ffafcb66c
Babak Mirghafari <babak.mirghafari@gmail.com>
2020-09-05 20:08:00 +0200

    Add comment

--- userregister/src/test/java/com/staxter/user/InitializerTestClass.java
+++ userregister/src/test/java/com/staxter/user/InitializerTestClass.java
@@ -7,17 +7,18 @@ import com.staxter.user.model.User;
 import com.staxter.user.repository.UserRepository;
 import com.staxter.user.service.UserService;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+/**
+ * This class is responsible for load and provide all preloaded and needed objects and configuration
+ */
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class InitializerTestClass {
@@ -37,9 +38,22 @@ public class InitializerTestClass {
 
     User user;
 
+    /**
+     * before all tests load and run this method run
+     * @throws Exception
+     */
     @Before
     protected  void setUp() throws Exception {
+        /**
+         * For testing REST end point , we need controller to map our request. As we know our Controller load in Dispatcher Servlet
+         * for mapping request. with this line of code we can building a DispatcherServlet( standalone DispatcherServlet)
+         * and all needed infrastructure for register our controller in this Dispatcher. because of this method run before all tests
+         *  ,we have a controller and can use this controller.
+         */
         this.mockMvc = standaloneSetup(this.userController).build();
+        /**
+         * create a user initiated object and provide it for all unit tests , that need this object.
+         */
         user=new User();
         user.setFirstName("Babak");
         user.setLastName("Mirghafari");
@@ -47,10 +61,16 @@ public class InitializerTestClass {
         user.setPlainTextPassword("Mirghafari");
     }
 
+    /**
+     * This method responsible for write user object to JSONObject. because of for test REST endpoint , we need JSONObject.
+     * @param user
+     * @return
+     */
     protected  static String writeObjectAsJasonString(final User user) {
 
         ObjectMapper mapper=new ObjectMapper();
         try {
+
             mapper.disable(MapperFeature.USE_ANNOTATIONS);
             return mapper.writeValueAsString(user);
         } catch (Exception e) {
--- userregister/src/test/java/com/staxter/user/UserControllerTest.java
+++ userregister/src/test/java/com/staxter/user/UserControllerTest.java
@@ -21,61 +21,96 @@ public class UserControllerTest extends InitializerTestClass {
     }
 
     /**
-     * check if user send RequestObject in request body ,RestEndPoint should return 200
-     * and if user don't send RequestObject in request body  ,RestEndPoint should return 400
-     * @throws Exception
+     * check HTTP_CODE 200 response, Also after 200 response should return user object with value
      */
     @Test
     public void testInsertNewUser() throws Exception{
-
         /**
-         * check 200 response, Also after 200 response should return user object with value
+         * send request to controller with expected object in request Body
          */
         mockMvc.perform(post(requestUrl).contentType(MediaType.APPLICATION_JSON)
                 .content(writeObjectAsJasonString(user)))
+                /**
+                 * Check HTTP_CODE 200
+                 */
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("id").isNotEmpty())
                 .andExpect(jsonPath("firstName").isNotEmpty())
                 .andExpect(jsonPath("lastName").isNotEmpty())
                 .andExpect(jsonPath("userName").isNotEmpty())
+                /**
+                 * this properties should be absent in return JSONObject
+                 */
                 .andExpect(jsonPath("plainTextPassword").doesNotExist())
                 .andExpect(jsonPath("hashedPassword").doesNotExist());
 
 
     }
 
+    /**
+     * check HTTP_ERROR_CODE 400
+     */
     @Test
     public void checkNullUserSent() throws Exception{
-        /**
-         * check 400 response
-         */
+
         mockMvc.perform(post(requestUrl).contentType(MediaType.APPLICATION_JSON)
                 .content(writeObjectAsJasonString(null)))
+                /**
+                 * Check HTTP_ERROR_CODE 400
+                 */
                 .andExpect(status().isBadRequest());
     }
 
+    /**
+     * Check {@link UserAlreadyExistException} and HTTP_ERROR_CODE 409
+     * because of this method return {@link UserAlreadyExistException} , this exception define as test parameter
+     * @throws Exception
+     */
     @Test(expected = UserAlreadyExistException.class)
     public void checkException() throws Exception{
         if (userRepository.findUserByUserName(user.getUserName()).isPresent()){
             try {
                 mockMvc.perform(post(requestUrl).contentType(MediaType.APPLICATION_JSON)
                         .content(writeObjectAsJasonString(user)))
+                        /**
+                         * Check HTTP_ERROR_CODE 409
+                         */
                         .andExpect(status().isConflict());
+                /**
+                 * Because of {@link UserAlreadyExistException} extend from {@link RuntimeException}, when method face to this exception
+                 * finally  throw {@link NestedServletException}. because of that , this method face to {@link NestedServletException}
+                 * instead of {@link UserAlreadyExistException}. for avoid this error , we can catch {@link NestedServletException}
+                 * and cast exception cause for our desired exception.
+                 */
             } catch (NestedServletException e){
+                /**
+                 * cast exception cause to {@link UserAlreadyExistException}
+                 */
                 throw (UserAlreadyExistException) e.getCause();
             }
         }
     }
 
+    /**
+     * Check {@link CheckUserPropertyNullValueException} and HTTP_ERROR_CODE 400
+     * because of this method return {@link CheckUserPropertyNullValueException} , this exception define as test parameter
+     * @throws Exception
+     */
     @Test(expected = CheckUserPropertyNullValueException.class)
     public void checkNullValueForUser() throws Exception{
         User user=new User();
         try {
             mockMvc.perform(post(requestUrl).contentType(MediaType.APPLICATION_JSON)
             .content(writeObjectAsJasonString(user)))
+                    /**
+                     * Check HTTP_ERROR_CODE 400
+                     */
             .andExpect(status().isBadRequest());
         } catch (NestedServletException e){
+            /**
+             * cast exception cause to {@link CheckUserPropertyNullValueException}
+             */
             throw (CheckUserPropertyNullValueException)e.getCause();
         }
     }
